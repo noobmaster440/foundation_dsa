8258708424ec04c860a5c05a08d33d7eb86f177b
noobmaster440 <saadsiddiqi440@gmail.com>
2019-09-21 00:20:15 +0530

    third committ

--- l1_intro/GreatestNo.cpp
+++ l1_intro/GreatestNo.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 using namespace std;
+//code for greates number
+int main(int args, char **argv)
+{
+  int a, b, c;
+  cout << "Enter a  ";
+  cin >> a;
+  cout << endl
+       << "Enter b  ";
+  cin >> b;
+  cout << endl
+       << "Enter c  ";
+  cin >> c;
 
-int main(int args,char** argv){
-  int a,b,c;
-  cout<<"Enter a  ";
-  cin>>a;
-  cout<<endl<<"Enter b  ";
-  cin>>b;
-  cout<<endl<<"Enter c  ";
-  cin>>c;
-
-  if(a>=b && a>c){
-    cout<<endl<<a;
-  }else if(b>a && b>=c){
-    cout<<b;
-  }else{
-    cout<<c;
+  if (a >= b && a > c)
+  {
+    cout << endl
+         << a;
+  }
+  else if (b > a && b >= c)
+  {
+    cout << b;
+  }
+  else
+  {
+    cout << c;
   }
 
   return 0;
